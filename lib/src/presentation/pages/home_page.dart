fa03fab8c8cb1ec348e9bfc5c76bf6e3cb45204a
vlaurent <valentin.laurent.dev@gmail.com>
2025-11-02 15:55:32 +0100

    feat: Add missing ScaffoldAnimatedGradient on homepage

--- lib/src/presentation/pages/home_page.dart
+++ lib/src/presentation/pages/home_page.dart
@@ -2,33 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tictactoebetclic/src/domain/states/game_notifier.dart';
-import 'package:tictactoebetclic/src/presentation/widgets/animated_gradient_background_widget.dart';
 import 'package:tictactoebetclic/src/presentation/widgets/brightness_button_widget.dart';
 import 'package:tictactoebetclic/src/presentation/widgets/call_to_action_button_widget.dart';
+import 'package:tictactoebetclic/src/presentation/widgets/scaffold_animated_gradient.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(
-        children: [
-          AnimatedGradientBackground(),
-          Padding(
-            padding: EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                GameTitle(),
-                SizedBox(height: 60),
-                PlayButton(),
-              ],
-            ),
-          ),
-          Positioned(top: 40, right: 16, child: BrightnessButton()),
-        ],
+    return ScaffoldAnimatedGradient(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: const [BrightnessButton()],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [GameTitle(), SizedBox(height: 60), PlayButton()],
+        ),
       ),
     );
   }
@@ -68,14 +63,13 @@ class PlayButton extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return
-      CtaButton(
-        onPressed: () {
-          ref.read(gameProvider.notifier).deactivateAI();
-          context.push('/home/levels');
-        },
-        icon: Icons.local_play_outlined,
-        label: 'Start a game',
-      );
+    return CtaButton(
+      onPressed: () {
+        ref.read(gameProvider.notifier).deactivateAI();
+        context.push('/home/levels');
+      },
+      icon: Icons.local_play_outlined,
+      label: 'Start a game',
+    );
   }
 }
